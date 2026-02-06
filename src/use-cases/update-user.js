914a1013c09bace31103d5ece781672919b708b3
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-06 15:08:45 -0300

    feat: add UpdateUserUseCase and PostgresUpdateUserRepository

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-id'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user'
+
+export class UpdateUserUseCase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvidedEmail) {
+                throw new Error('Email já cadastrado')
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+            user.password = hashedPassword
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+        const updatedUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user,
+        )
+
+        return updatedUser
+    }
+}

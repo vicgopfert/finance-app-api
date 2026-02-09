a04cd7c5f55a7cc25b95d00c350d69ad764cf1e6
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-09 14:36:54 -0300

    feat: add PostgresDeleteUserRepository

---
+++ src/repositories/postgres/delete-user.js
@@ -0,0 +1,15 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresDeleteUserRepository {
+    async execute(userId) {
+        const deletedUser = await PostgresHelper.query(
+            `
+            DELETE FROM users 
+            WHERE id = $1 
+            RETURNING *;
+            `,
+            [userId],
+        )
+        return deletedUser[0]
+    }
+}

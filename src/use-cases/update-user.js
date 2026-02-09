d986382baba6b2d63b9d1d6645fd637bd23cced8
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-09 14:25:43 -0300

    feat: add index.js file to use cases and repositories

--- src/use-cases/update-user.js
+++ src/use-cases/update-user.js
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt'
-import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
-import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+import {
+    PostgresGetUserByEmailRepository,
+    PostgresUpdateUserRepository,
+} from '../repositories/postgres/index.js'
 import { badRequest } from '../controllers/helpers/http.js'
 
 export class UpdateUserUseCase {

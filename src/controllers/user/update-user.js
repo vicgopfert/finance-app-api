5ecb12c974c192aaf10cd6549df064e96d553b64
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-11 14:52:23 -0300

    refactor: move user controllers to its own folder

--- src/controllers/user/update-user.js
+++ src/controllers/user/update-user.js
@@ -1,4 +1,4 @@
-import { EmailAlreadyInUseError } from '../errors/user.js'
+import { EmailAlreadyInUseError } from '../../errors/user.js'
 import {
     checkIfEmailIsValid,
     checkIfIdIsValid,
@@ -10,7 +10,7 @@ import {
     ok,
     serverError,
     userNotFoundResponse,
-} from './helpers/index.js'
+} from '../helpers/index.js'
 
 export class UpdateUserController {
     constructor(updateUserUseCase) {

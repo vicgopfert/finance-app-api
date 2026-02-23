39368e10e584a9bc3fbf08ae3529cb4faa4aadc9
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-23 14:44:28 -0300

    refactor: add validate required fields helper function

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -8,3 +8,25 @@ export const invalidIdResponse = (id) => {
         message: `The provided ID ${id} is invalid.`,
     })
 }
+
+export const checkIfIsString = (value) => typeof value === 'string'
+
+export const validateRequiredFields = (params, requiredFields) => {
+    for (const field of requiredFields) {
+        const fieldIsMissing = !params[field]
+        const fieldIsEmptyString =
+            checkIfIsString(params[field]) &&
+            validator.isEmpty(params[field], {
+                ignore_whitespace: true,
+            })
+
+        if (fieldIsMissing || fieldIsEmptyString) {
+            return {
+                missingField: field,
+                ok: false,
+            }
+        }
+    }
+
+    return { missingField: undefined, ok: true }
+}
--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -7,6 +7,7 @@ import {
     badRequest,
     created,
     serverError,
+    validateRequiredFields,
 } from '../helpers/index.js'
 
 export class CreateUserController {
@@ -25,12 +26,13 @@ export class CreateUserController {
                 'password',
             ]
 
-            for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length === 0) {
-                    return badRequest({
-                        message: `Field ${field} is required.`,
-                    })
-                }
+            const { ok: requiredFieldsWereProvided, missingField } =
+                validateRequiredFields(params, requiredFields)
+
+            if (!requiredFieldsWereProvided) {
+                return badRequest({
+                    message: `Field ${missingField} is required.`,
+                })
             }
 
             const passwordIsValid = checkIfPasswordIsValid(params.password)

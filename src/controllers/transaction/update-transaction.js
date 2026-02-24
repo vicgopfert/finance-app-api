36c4b9e182c5b0ad8873452a3cb59f268dabd950
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-24 13:13:11 -0300

    feat: add UpdateTransactionController

---
+++ src/controllers/transaction/update-transaction.js
@@ -0,0 +1,73 @@
+import {
+    badRequest,
+    checkIfAmountIsValid,
+    checkIfIdIsValid,
+    checkIfTypeIsValid,
+    invalidAmountResponse,
+    invalidIdResponse,
+    invalidTypeResponse,
+    ok,
+    serverError,
+} from '../helpers'
+
+export class UpdateTransactionController {
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const transactionId = httpRequest.params.id
+
+            const idIsValid = checkIfIdIsValid(transactionId)
+
+            if (!idIsValid) {
+                return invalidIdResponse(transactionId)
+            }
+
+            const params = httpRequest.body
+
+            const allowedFields = ['name', 'date', 'amount', 'type']
+
+            const someFieldIsNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            )
+
+            if (someFieldIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided field is not allowed to be updated',
+                })
+            }
+
+            if (params.amount) {
+                const amountIsValid = checkIfAmountIsValid(params.amount)
+
+                if (!amountIsValid) {
+                    return invalidAmountResponse(params.amount)
+                }
+            }
+
+            if (params.type) {
+                const typeIsValid = checkIfTypeIsValid(params.type)
+
+                if (!typeIsValid) {
+                    return invalidTypeResponse(params.type)
+                }
+            }
+
+            const updatedTransaction =
+                await this.updateTransactionUseCase.execute(
+                    transactionId,
+                    params,
+                )
+
+            return ok({
+                message: 'Transaction updated successfully',
+                transaction: updatedTransaction,
+            })
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}

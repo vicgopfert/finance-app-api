8c35959cba0223c1babd3c685fca557bfed65c4a
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-25 14:01:32 -0300

    feat: update execMigrations script to read all sql files

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -10,12 +10,21 @@ const __dirname = path.dirname(__filename)
 const execMigrations = async () => {
     const client = await pool.connect()
     try {
-        const filePath = path.join(__dirname, '01-init.sql')
-        const script = fs.readFileSync(filePath)
-        await client.query(script.toString())
-        console.log('Migrações executadas com sucesso')
+        const files = fs
+            .readdirSync(__dirname)
+            .filter((file) => file.endsWith('.sql'))
+            .sort()
+
+        for (const file of files) {
+            const filePath = path.join(__dirname, file)
+            const script = fs.readFileSync(filePath)
+            await client.query(script.toString())
+            console.log(`Migration for file ${file} executed successfully`)
+        }
+
+        console.log('All Migrations were executed successfully')
     } catch (error) {
-        console.error('Erro ao executar migrações:', error)
+        console.error('Error executing migrations:', error)
     } finally {
         client.release()
     }

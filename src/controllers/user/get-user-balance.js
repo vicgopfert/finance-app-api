0d126f2ca3601cfb6c38310080fac139bba97ee9
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-25 11:45:24 -0300

    feat: add GetUserBalanceController

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,36 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers/index.js'
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.id
+
+            const idIsValid = checkIfIdIsValid(userId)
+
+            if (!idIsValid) {
+                return invalidIdResponse(userId)
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({ userId })
+
+            return ok(balance)
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+            console.error('Error getting user balance:', error)
+            serverError({ message: 'Internal server error' })
+        }
+    }
+}

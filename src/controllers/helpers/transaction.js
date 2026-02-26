432f41b0fbc3b34fa9324f89adcc38f77630a036
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-26 13:09:47 -0300

    refactor: remove unused validation functions from transaction helper

--- src/controllers/helpers/transaction.js
+++ src/controllers/helpers/transaction.js
@@ -1,42 +1,4 @@
-import validator from 'validator'
-import { badRequest, notFound } from './http.js'
-
-export const checkIfAmountIsValid = (amount) => {
-    if (typeof amount !== 'number' || isNaN(amount)) {
-        return false
-    }
-
-    return validator.isCurrency(amount.toFixed(2), {
-        digits_after_decimal: [2],
-        allow_negatives: false,
-        decimal_separator: '.',
-    })
-}
-
-export const checkIfTypeIsValid = (type) => {
-    return ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type)
-}
-
-export const invalidAmountResponse = (amount) => {
-    return badRequest({
-        message: `The provided amount ${amount} is invalid. Amount must be a valid currency value.`,
-    })
-}
-
-export const invalidTypeResponse = (type) => {
-    return badRequest({
-        message: `The provided type ${type} is invalid. Type must be one of EARNING, EXPENSE, or INVESTMENT.`,
-    })
-}
-
-export const checkIfHasMoreThanTwoDecimals = (amount) =>
-    !Number.isInteger(amount * 100)
-
-export const invalidAmountDecimalsResponse = (amount) => {
-    return badRequest({
-        message: `The provided amount ${amount} is invalid. Amount must have at most 2 decimal places.`,
-    })
-}
+import { notFound } from './http.js'
 
 export const transactionNotFoundResponse = () => {
     return notFound({

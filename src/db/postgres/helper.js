f18ac7cc7c03734c238e82793c56d58fcf1f7787
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-26 10:49:44 -0300

    chore: update postgres connection and docker compose configuration to use environment variables

--- src/db/postgres/helper.js
+++ src/db/postgres/helper.js
@@ -4,12 +4,11 @@ const { Pool } = pg
 
 // cria o pool de conexões
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
     // configs
     max: 10, // máximo de conexões no pool
     idleTimeoutMillis: 30000, // tempo ocioso antes de fechar conexão

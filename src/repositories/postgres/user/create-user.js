c9197f3e7b7352b5004645fd4f365362026edc00
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-27 11:42:28 -0300

    feat: ensure DeleteUser and GetUserById repositories use Prisma

--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -1,4 +1,5 @@
 import { prisma } from '../../../../prisma/prisma.js'
+
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
         const user = await prisma.user.create({
--- src/repositories/postgres/user/get-user-by-email.js
+++ src/repositories/postgres/user/get-user-by-email.js
@@ -1,4 +1,5 @@
 import { prisma } from '../../../../prisma/prisma.js'
+
 export class PostgresGetUserByEmailRepository {
     async execute(email) {
         const user = await prisma.user.findUnique({

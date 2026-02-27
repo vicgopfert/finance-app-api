57d04054d76f7044def0ce2ea4acf55bf90f124f
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-02-27 10:35:04 -0300

    feat: add postgres adapter with prisma and update dependencies

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,3 +1,12 @@
+import 'dotenv/config'
+import pg from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
+
+export const prisma = new PrismaClient({ adapter })

80a10b5978fbdc996c49ad16e4a798cdecb61d4d
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-03-03 12:43:55 -0300

    test: ensure 400 is returned if email is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -32,6 +32,7 @@ describe('Create User Controller', () => {
         expect(result.body).toHaveProperty('user', httpRequest.body)
     })
 
+    // Status 400 when some field is not provided
     it('should returns 400 if first_name is not provided', async () => {
         const createUserUseCaseStub = new CreateUserUseCaseStub()
         const createUserController = new CreateUserController(
@@ -121,6 +122,56 @@ describe('Create User Controller', () => {
         expect(result.body).toHaveProperty(
             'message',
             'The field password is not provided.',
+        )
+    })
+
+    // Status 400 when email is not valid
+    it('should returns 400 if email is not valid', async () => {
+        const createUserUseCaseStub = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(
+            createUserUseCaseStub,
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'john.doeexample.com',
+                password: 'password123',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+        expect(result.body).toHaveProperty(
+            'message',
+            'Please provide a valid email address',
+        )
+    })
+
+    // Status 400 when password is less than 6 characters
+    it('should returns 400 if password is less than 6 characters', async () => {
+        const createUserUseCaseStub = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(
+            createUserUseCaseStub,
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'john.doe@example.com',
+                password: 'pass',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+        expect(result.body).toHaveProperty(
+            'message',
+            'Password must be at least 6 characters long',
         )
     })
 })

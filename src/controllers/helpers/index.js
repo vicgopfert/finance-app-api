4708f2f8cbbcd9708a754010f3b5e6f685ef63e2
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-03-09 12:07:39 -0300

    refactor: remove unused code from helpers

--- src/controllers/helpers/index.js
+++ src/controllers/helpers/index.js
@@ -1,3 +1,2 @@
 export * from './http.js'
-export * from './user.js'
 export * from './validation.js'
--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -12,25 +12,3 @@ export const requiredFieldIsMissingResponse = (field) =>
     badRequest({
         message: `Field ${field} is required.`,
     })
-
-export const checkIfIsString = (value) => typeof value === 'string'
-
-export const validateRequiredFields = (params, requiredFields) => {
-    for (const field of requiredFields) {
-        const fieldIsMissing = !params[field]
-        const fieldIsEmptyString =
-            checkIfIsString(params[field]) &&
-            validator.isEmpty(params[field], {
-                ignore_whitespace: true,
-            })
-
-        if (fieldIsMissing || fieldIsEmptyString) {
-            return {
-                missingField: field,
-                ok: false,
-            }
-        }
-    }
-
-    return { missingField: undefined, ok: true }
-}

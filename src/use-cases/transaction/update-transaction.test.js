fc54ae2f5cb621514f31eea85ab3b2da4a2aa1e3
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-08 10:11:48 -0300

    test: add UpdateTransactionUseCase tests for successful update and error handling

---
+++ src/use-cases/transaction/update-transaction.test.js
@@ -0,0 +1,97 @@
+import { UpdateTransactionUseCase } from './update-transaction.js'
+import { TransactionNotFoundError } from '../../errors/transaction.js'
+import { faker } from '@faker-js/faker'
+
+describe('Update Transaction Use Case', () => {
+    class UpdateTransactionRepositoryStub {
+        async execute() {
+            return transaction
+        }
+    }
+
+    const makeSut = () => {
+        const updateTransactionRepository =
+            new UpdateTransactionRepositoryStub()
+        const sut = new UpdateTransactionUseCase(updateTransactionRepository)
+
+        return {
+            sut,
+            updateTransactionRepository,
+        }
+    }
+
+    const transaction = {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        date: faker.date.recent().toISOString(),
+        type: 'EXPENSE',
+        amount: Number(faker.finance.amount(0.01, 10000, 2)),
+    }
+
+    const updateTransactionParams = {
+        name: faker.commerce.productName(),
+        date: faker.date.recent().toISOString(),
+        type: 'EARNING',
+        amount: Number(faker.finance.amount(0.01, 10000, 2)),
+    }
+
+    it('should update a transaction successfully', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(
+            faker.string.uuid(),
+            updateTransactionParams,
+        )
+
+        expect(result).toEqual(transaction)
+    })
+
+    it('should call UpdateTransactionRepository with correct params', async () => {
+        const { sut, updateTransactionRepository } = makeSut()
+
+        const updateTransactionSpy = jest.spyOn(
+            updateTransactionRepository,
+            'execute',
+        )
+
+        const transactionId = faker.string.uuid()
+        await sut.execute(transactionId, updateTransactionParams)
+
+        expect(updateTransactionSpy).toHaveBeenCalledWith(
+            transactionId,
+            updateTransactionParams,
+        )
+    })
+
+    it('should throw TransactionNotFoundError if UpdateTransactionRepository returns null', async () => {
+        const { sut, updateTransactionRepository } = makeSut()
+
+        jest.spyOn(
+            updateTransactionRepository,
+            'execute',
+        ).mockResolvedValueOnce(null)
+
+        const transactionId = faker.string.uuid()
+        const promise = sut.execute(transactionId, updateTransactionParams)
+
+        await expect(promise).rejects.toThrow(
+            new TransactionNotFoundError(transactionId),
+        )
+    })
+
+    it('should throw if UpdateTransactionRepository throws', async () => {
+        const { sut, updateTransactionRepository } = makeSut()
+
+        jest.spyOn(updateTransactionRepository, 'execute').mockRejectedValue(
+            new Error(),
+        )
+
+        const promise = sut.execute(
+            faker.string.uuid(),
+            updateTransactionParams,
+        )
+
+        await expect(promise).rejects.toThrow(new Error())
+    })
+})

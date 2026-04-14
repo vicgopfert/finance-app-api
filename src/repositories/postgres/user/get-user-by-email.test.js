8bcc7de4a22e540d01ee84e434cbb39c5d6fa978
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-14 11:05:11 -0300

    test: add tests for PostgresGetUserByEmailRepository

---
+++ src/repositories/postgres/user/get-user-by-email.test.js
@@ -0,0 +1,29 @@
+import { prisma } from '../../../../prisma/prisma'
+import { user } from '../../../tests'
+import { PostgresGetUserByEmailRepository } from './get-user-by-email'
+
+describe('Get User By Email Repository', () => {
+    it('should get user by email on db', async () => {
+        await prisma.user.create({
+            data: user,
+        })
+        const sut = new PostgresGetUserByEmailRepository()
+
+        const result = await sut.execute(user.email)
+
+        expect(result).toStrictEqual(user)
+    })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByEmailRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+
+        await sut.execute(user.email)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                email: user.email,
+            },
+        })
+    })
+})

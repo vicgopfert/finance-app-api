244851d8c2b7532877c76f1f6e22071ee852affa
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-14 11:13:33 -0300

    test: ensure PostgresUpdateUserRepository updates user on db

---
+++ src/repositories/postgres/user/update-user.test.js
@@ -0,0 +1,29 @@
+import { PostgresUpdateUserRepository } from './update-user.js'
+import { prisma } from '../../../../prisma/prisma.js'
+import { user as fakeUser } from '../../../tests'
+import { faker } from '@faker-js/faker'
+
+describe('Update User Repository', () => {
+    it('should update a user on db', async () => {
+        const user = await prisma.user.create({
+            data: fakeUser,
+        })
+
+        const sut = new PostgresUpdateUserRepository()
+
+        const updateUserParams = {
+            id: faker.string.uuid(),
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        }
+
+        const result = await sut.execute(user.id, updateUserParams)
+
+        expect(result).toStrictEqual({
+            ...user,
+            ...updateUserParams,
+        })
+    })
+})

30699f35d05422e0face7065d0e3a6b3c14cc472
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-14 11:25:31 -0300

    test: ensure PostgresCreateUserRepository throws if Prisma throws

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -21,4 +21,13 @@ describe('Create User Repository', () => {
             data: user,
         })
     })
+
+    it('should throw if Prisma throws an error', async () => {
+        const sut = new PostgresCreateUserRepository()
+        jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(new Error())
+
+        const promise = sut.execute(user)
+
+        await expect(promise).rejects.toThrow()
+    })
 })

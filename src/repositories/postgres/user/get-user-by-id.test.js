46894f2d8565d80ecd9a1ac330251d0f2abb6aa0
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-14 11:05:25 -0300

    test: add tests for PostgresGetUserByIdRepository

---
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -0,0 +1,29 @@
+import { prisma } from '../../../../prisma/prisma'
+import { user } from '../../../tests'
+import { PostgresGetUserByIdRepository } from './get-user-by-id'
+
+describe('Get User By ID Repository', () => {
+    it('should get user by id on db', async () => {
+        await prisma.user.create({
+            data: user,
+        })
+        const sut = new PostgresGetUserByIdRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toStrictEqual(user)
+    })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresGetUserByIdRepository()
+        const prismaSpy = jest.spyOn(prisma.user, 'findUnique')
+
+        await sut.execute(user.id)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: user.id,
+            },
+        })
+    })
+})

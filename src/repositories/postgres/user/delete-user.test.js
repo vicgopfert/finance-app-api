b22515a358daea1c05ec29a657ad5fe2d54382d6
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-14 10:29:40 -0300

    test: ensure PostgresDeleteUserRepository deletes a user on db

---
+++ src/repositories/postgres/user/delete-user.test.js
@@ -0,0 +1,16 @@
+import { prisma } from '../../../../prisma/prisma'
+import { user } from '../../../tests'
+import { PostgresDeleteUserRepository } from './delete-user'
+
+describe('Delete User Repository', () => {
+    it('should delete a user on db', async () => {
+        await prisma.user.create({
+            data: user,
+        })
+        const sut = new PostgresDeleteUserRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toStrictEqual(user)
+    })
+})

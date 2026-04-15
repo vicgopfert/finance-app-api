55507c6680b1835895681685ed01254414200951
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 09:51:26 -0300

    test: ensure PostgresDeleteTransactionRepository deletes a transaction on db

---
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -0,0 +1,26 @@
+import { PostgresDeleteTransactionRepository } from './delete-transaction'
+import { transaction, user } from '../../../tests'
+import { prisma } from '../../../../prisma/prisma'
+import dayjs from 'dayjs'
+
+describe('Delete Transaction Repository', () => {
+    it('should delete a transaction successfully', async () => {
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({
+            data: { ...transaction, user_id: user.id },
+        })
+        const sut = new PostgresDeleteTransactionRepository()
+
+        const result = await sut.execute(transaction.id)
+
+        expect(result.name).toBe(transaction.name)
+        expect(String(result.amount)).toBe(String(transaction.amount))
+        expect(result.type).toBe(transaction.type)
+        expect(result.user_id).toBe(user.id)
+        expect(dayjs(result.date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        )
+        expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
+        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
+    })
+})

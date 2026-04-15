4df2a729713d37b119d6dbf06208ae64afc1859f
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 11:32:29 -0300

    test: ensure generateGetUserByIdController returns valid GetUserByIdController

--- src/factories/controllers/user.test.js
+++ src/factories/controllers/user.test.js
@@ -1,10 +1,19 @@
-import { generateCreateUserController } from './user'
-import { CreateUserController } from '../../controllers'
+import {
+    generateCreateUserController,
+    generateGetUserByIdController,
+} from './user'
+import { CreateUserController, GetUserByIdController } from '../../controllers'
 
 describe('User Controller Factories', () => {
     it('should return a valid CreateUserController instance', () => {
         expect(generateCreateUserController()).toBeInstanceOf(
             CreateUserController,
+        )
+    })
+
+    it('should return a valid GetUserByIdController instance', () => {
+        expect(generateGetUserByIdController()).toBeInstanceOf(
+            GetUserByIdController,
         )
     })
 })

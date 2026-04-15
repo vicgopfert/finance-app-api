8d85330837dbd6f23a952898a96abb42d09f2911
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 10:06:22 -0300

    test: ensure PostgresGetTransactionsByUserIdRepository gets user transactions on db

---
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.test.js
@@ -0,0 +1,31 @@
+import { prisma } from '../../../../prisma/prisma'
+import { transaction, user } from '../../../tests'
+import { PostgresGetTransactionsByUserIdRepository } from './get-transactions-by-user-id'
+import dayjs from 'dayjs'
+
+describe('Get Transactions By User ID Repository', () => {
+    it('should get transactions by user id on db', async () => {
+        const sut = new PostgresGetTransactionsByUserIdRepository()
+        await prisma.user.create({ data: user })
+        await prisma.transaction.create({
+            data: { ...transaction, user_id: user.id },
+        })
+
+        const result = await sut.execute(user.id)
+
+        expect(result.length).toBe(1)
+        expect(result[0].name).toBe(transaction.name)
+        expect(String(result[0].amount)).toBe(String(transaction.amount))
+        expect(result[0].type).toBe(transaction.type)
+        expect(result[0].user_id).toBe(user.id)
+        expect(dayjs(result[0].date).daysInMonth()).toBe(
+            dayjs(transaction.date).daysInMonth(),
+        )
+        expect(dayjs(result[0].date).month()).toBe(
+            dayjs(transaction.date).month(),
+        )
+        expect(dayjs(result[0].date).year()).toBe(
+            dayjs(transaction.date).year(),
+        )
+    })
+})

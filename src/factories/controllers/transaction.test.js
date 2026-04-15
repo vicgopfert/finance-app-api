512e62951a43f0387ae1ab1012a3f5b69f1f6cba
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 11:22:39 -0300

    test: ensure generateUpdateTransactionController returns valid UpdateTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,22 @@
-import { generateCreateTransactionController } from './transaction'
-import { CreateTransactionController } from '../../controllers'
+import {
+    generateCreateTransactionController,
+    generateUpdateTransactionController,
+} from './transaction'
+import {
+    CreateTransactionController,
+    UpdateTransactionController,
+} from '../../controllers'
 
 describe('Transaction Controller Factories', () => {
     it('should return a valid CreateTransactionController instance', () => {
         expect(generateCreateTransactionController()).toBeInstanceOf(
             CreateTransactionController,
+        )
+    })
+
+    it('should return a valid UpdateTransactionController instance', () => {
+        expect(generateUpdateTransactionController()).toBeInstanceOf(
+            UpdateTransactionController,
         )
     })
 })

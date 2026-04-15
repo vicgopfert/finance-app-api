f687485be85712385e9bf99f141c2bddf8bbd67c
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 11:24:21 -0300

    test: ensure generateDeleteTransactionController returns valid DeleteTransactionController

--- src/factories/controllers/transaction.test.js
+++ src/factories/controllers/transaction.test.js
@@ -1,10 +1,12 @@
 import {
     generateCreateTransactionController,
     generateUpdateTransactionController,
+    generateDeleteTransactionController,
 } from './transaction'
 import {
     CreateTransactionController,
     UpdateTransactionController,
+    DeleteTransactionController,
 } from '../../controllers'
 
 describe('Transaction Controller Factories', () => {
@@ -17,6 +19,12 @@ describe('Transaction Controller Factories', () => {
     it('should return a valid UpdateTransactionController instance', () => {
         expect(generateUpdateTransactionController()).toBeInstanceOf(
             UpdateTransactionController,
+        )
+    })
+
+    it('should return a valid DeleteTransactionController instance', () => {
+        expect(generateDeleteTransactionController()).toBeInstanceOf(
+            DeleteTransactionController,
         )
     })
 })

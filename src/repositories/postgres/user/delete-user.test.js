43c8fb2de427348e21885cb1b856586c4cf9473c
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 11:12:48 -0300

    test: ensure PostgresDeleteUserRepository throws if Prisma throws and return null if user not exist

--- src/repositories/postgres/user/delete-user.test.js
+++ src/repositories/postgres/user/delete-user.test.js
@@ -14,7 +14,19 @@ describe('Delete User Repository', () => {
         expect(result).toStrictEqual(user)
     })
 
+    it('should return null if user to delete does not exist', async () => {
+        const sut = new PostgresDeleteUserRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toBeNull()
+    })
+
     it('should call Prisma with correct params', async () => {
+        await prisma.user.create({
+            data: user,
+        })
+
         const sut = new PostgresDeleteUserRepository()
         const prismaSpy = jest.spyOn(prisma.user, 'delete')
 
@@ -25,5 +37,17 @@ describe('Delete User Repository', () => {
                 id: user.id,
             },
         })
+    })
+
+    it('should throw if Prisma throws an error', async () => {
+        await prisma.user.create({
+            data: user,
+        })
+        const sut = new PostgresDeleteUserRepository()
+        jest.spyOn(prisma.user, 'delete').mockRejectedValueOnce(new Error())
+
+        const promise = sut.execute(user.id)
+
+        await expect(promise).rejects.toThrow()
     })
 })

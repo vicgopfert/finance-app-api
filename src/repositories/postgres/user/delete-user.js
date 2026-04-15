9598075579b71c39eeff1de33c1cc8257164e399
Victor Gopfert <vferreira@detro.rj.gov.br>
2026-04-15 11:11:51 -0300

    refactor: update PostgresDeleteUserRepository to check for user existence before deletion

--- src/repositories/postgres/user/delete-user.js
+++ src/repositories/postgres/user/delete-user.js
@@ -2,14 +2,22 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
-        try {
-            const deletedUser = await prisma.user.delete({
-                where: { id: userId },
-            })
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        })
 
-            return deletedUser
-        } catch (error) {
+        if (!user) {
             return null
         }
+
+        await prisma.user.delete({
+            where: {
+                id: userId,
+            },
+        })
+
+        return user
     }
 }

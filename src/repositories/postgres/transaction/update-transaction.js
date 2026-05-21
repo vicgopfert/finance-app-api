68369cd42e59a723c0e99bb2d7769879afcaf78d
Victor <vferreira@detro.rj.gov.br>
2026-05-21 13:06:17 -0300

    refactor: improve update repositories not found handling

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -2,6 +2,16 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionsParams) {
+        const transaction = await prisma.transaction.findUnique({
+            where: {
+                id: transactionId,
+            },
+        })
+
+        if (!transaction) {
+            return null
+        }
+
         const updatedTransaction = await prisma.transaction.update({
             where: {
                 id: transactionId,
--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -2,11 +2,23 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateUserRepository {
     async execute(userId, updateUserParams) {
-        const user = await prisma.user.update({
-            where: { id: userId },
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId,
+            },
+        })
+
+        if (!user) {
+            return null
+        }
+
+        const updatedUser = await prisma.user.update({
+            where: {
+                id: userId,
+            },
             data: updateUserParams,
         })
 
-        return user
+        return updatedUser
     }
 }

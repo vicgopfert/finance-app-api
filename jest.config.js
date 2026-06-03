e894bbdbe0d80cd04fe173b9c7d78656ec310be6
Victor Gopfert <gopfert.web@gmail.com>
2026-06-03 13:57:53 -0300

    chore: remove babel dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,14 @@
 export default {
     testEnvironment: 'node',
     clearMocks: true,
+    verbose: true,
+    testMatch: ['**/src/**/*.test.js'],
+
     collectCoverage: true,
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.js', '!src/**/*.test.js'],
-    testMatch: ['**/src/**/*.test.js'],
-    verbose: true,
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
     transformIgnorePatterns: [],
+
     // globalSetup: '<rootDir>/jest.global-setup.js', (quando usar o docker para rodar os testes)
     setupFilesAfterEnv: ['<rootDir>/jest.setup-after-env.js'],
 }

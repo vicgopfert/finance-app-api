8825720f920839fe07aed3c5e4f3bbf4c4012323
Victor Gopfert <gopfert.web@gmail.com>
2026-06-15 15:17:29 -0300

    feat(auth): add LoginUserUseCase and JWT token generation

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,2 +1,4 @@
 export * from './password-hasher.js'
+export * from './password-comparator.js'
 export * from './id-generator.js'
+export * from './tokens-generator.js'
---
+++ src/use-cases/user/login-user.js
@@ -0,0 +1,35 @@
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js'
+
+export class LoginUserUseCase {
+    constructor(
+        getUserByEmailRepository,
+        passwordComparatorAdapter,
+        tokensGeneratorAdapter,
+    ) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.passwordComparatorAdapter = passwordComparatorAdapter
+        this.tokensGeneratorAdapter = tokensGeneratorAdapter
+    }
+
+    async execute(email, password) {
+        const user = await this.getUserByEmailRepository.execute(email)
+        if (!user) {
+            throw new UserNotFoundError()
+        }
+
+        const isPasswordValid = await this.passwordComparatorAdapter.execute(
+            password,
+            user.password,
+        )
+        if (!isPasswordValid) {
+            throw new InvalidPasswordError()
+        }
+
+        const tokens = await this.tokensGeneratorAdapter.execute(user.id)
+
+        return {
+            ...user,
+            tokens,
+        }
+    }
+}

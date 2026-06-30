b65d75a797fd4cdc39ca2e9ef1d548961634738b
Victor Gopfert <gopfert.web@gmail.com>
2026-06-30 12:21:49 -0300

    refactor: move login and refresh token use cases to /auth

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -3,10 +3,11 @@ export * from './user/update-user.js'
 export * from './user/get-user-by-id.js'
 export * from './user/delete-user.js'
 export * from './user/get-user-balance.js'
-export * from './user/login-user.js'
-export * from './user/refresh-token.js'
 
 export * from './transaction/create-transaction.js'
 export * from './transaction/get-transactions-by-user-id.js'
 export * from './transaction/update-transaction.js'
 export * from './transaction/delete-transaction.js'
+
+export * from './auth/login-user.js'
+export * from './auth/refresh-token.js'
